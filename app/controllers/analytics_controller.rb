e1447bdc0e7ad44ff8beb893a4a2cf88d30bdbd1
Jake <jakeherman@outlook.com>
2019-08-13 15:53:48 -0500

    timeline destroy

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,4 +3,8 @@ class AnalyticsController < ApplicationController
     @all_tls = Timeline.all
     @all_tracks = Track.all
   end
+
+  def destroy_timeline
+    Timline.where(:creator => params[:user]).destroy_all
+  end
 end
