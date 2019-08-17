6826691a181debe03e58d37e6b100132d5c11471
Jake <jakeherman@outlook.com>
2019-08-16 22:33:15 -0500

    fixing cam lol

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -2,10 +2,16 @@ class AnalyticsController < ApplicationController
   def index
     @all_tls = Timeline.all
     @all_tracks = Track.all
+    @all_moms = Moment.all
   end
 
   def destroy_timeline
     Timline.where(:creator => params[:user]).destroy_all
+    redirect_to :action => 'index'
+  end
+
+  def destroy_moment
+    Moment.where(:name => params[:moment], :user => params[:user]).destroy_all
     redirect_to :action => 'index'
   end
 end
